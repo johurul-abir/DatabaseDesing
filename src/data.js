cb91470fba1673f4fffb71df9d54d32b23d0a438
Johurul Islam <mdjohurulislam555@gmail.com>
2023-06-19 23:05:04 +0600

    data desing

---
+++ src/data.js
@@ -0,0 +1,302 @@
+// facebook post data stacture
+
+const facebookPostData =[ 
+
+ //=================facebook post no 1===================//
+    {
+
+    id          :2,
+    PostContant :"Leo Messi at Luis Suarez’ restaurant in Barcelona!",
+    imageVideo  :"https://scontent.fdac19-1.fna.fbcdn.net/v/t39.30808-6/354577099_2441887135984769_2506598943639715312_n.jpg?stp=dst-jpg_s600x600&_nc_cat=1&ccb=1-7&_nc_sid=5cd70e&_nc_eui2=AeHMaqFUQJbLr3tN2wwQGtEblGKPoEMZtNWUYo-gQxm01YRNoodAGLEORMxKy5ZK1XIYPa6VLGcxDkZfxOj-3qt8&_nc_ohc=FKJcvix6aSkAX_3os9-&_nc_oc=AQlWlrrcOhUxAj_ExSUTbk3LEQfGX7muauaSOWNOnEhxfkj33XyGdz83C6n8gKBtJe0&_nc_ht=scontent.fdac19-1.fna&oh=00_AfCVI_OIPMDramOtL7k_JIYpVQumekd7kpfnBqxkqXioHQ&oe=64954474",
+
+    NoOfreact   : "35000",
+    postStatus  : "public/private",
+
+    // ============comment start============//
+    comments    :[
+
+         // ============comment no -1============//
+        {
+            id         : 1,
+            name       : "Derek Cockbain",
+            commentContent: "Does he serve ear there?",
+            commentsReact:11,
+            commentTime: "30m ago",
+             // ============Reply start============//
+            commemtReply: [
+            {
+                Id      : 1,
+                Name :"Arvin Liberato",
+                replyContant:"Derek Cockbain nah, all menu items are bite size 😒",
+                replyReact:4,
+                replyTime:"7h ago",
+                replyShare: 2,
+
+            }, 
+
+            {
+                id      : 2,
+                Name  : "Dan Maginnis",
+                replyContant:"Derek Cockbain Recently Severed Ear, in a white wine and garlic sauce. Mike Tyson gives it 5 stars.",
+                replyReact:4,
+                replyTime:"5h ago",
+                replyShare: 0,
+
+            }, 
+
+            ],
+
+             // ============reply end============//
+        },
+
+
+    // ============comment no-2============//
+        {
+            id         : 2,
+            name       : "Tore Berntsen",
+            commentContent: "Where in Barca is that ?",
+            commentsReact:1,
+            commentTime: "35m ago",
+            commemtReply: [
+                 // ============Reply start============//
+            {
+                Id      : 1,
+                Name :"Ivan Razumtsev",
+                replyContant:"Tore Berntsen Castelldefels",
+                replyReact:0,
+                replyTime:"51m ago",
+                replyShare: 0,
+
+            }, 
+ 
+            // ============Reply end============//
+
+            ],
+        },
+    // ============Comment end============//
+
+    ],
+    PostShare: ["jahid","hasan","walid"],
+    author    :{
+        authorName:"Kawsir Hossain",
+        profilePic: "https://scontent.fdac19-1.fna.fbcdn.net/v/t1.6435-9/64618358_2282460041848003_6967510150475153408_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEHEAdtzZfQffwiS_P0V5k6Cq0bTSrvuC4KrRtNKu-4LqMTCGXtCJuSELtC42sugwe9-hBYwnr4v20fxgaWglrq&_nc_ohc=Z5D8hN8y7RgAX87JFhi&_nc_ht=scontent.fdac19-1.fna&oh=00_AfDpsMAPY4oxt--O2RU5Bv6ponYOUqmuPVZ7Pxzud4yaTA&oe=64B7855A",
+        postTime: "June 18, 2019 ",
+        
+    }
+
+   
+},
+
+
+ //=================facebook post no 2===================//
+
+
+{
+
+    id          :3,
+    PostContant :"নেদারল্যান্ডসের রটারড্যাম ডি কুইপ স্টেডিয়ামে স্পেনের বিপক্ষে ফাইনাল হারের পর এমন বলেছেন।",
+    imageVideo  :"https://scontent.fdac19-1.fna.fbcdn.net/v/t39.30808-6/354613286_649212687233961_8484626539740645331_n.jpg?stp=dst-jpg_p526x296&_nc_cat=109&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGUEA4UPPlRHQCizhXpAa32LqzVPSPTaLUurNU9I9NotYs_P6o2psBxcP7CkHgBxj60YpAzdY9lgG4Nk-5k4swE&_nc_ohc=gxrBArKvFCEAX9WXbB7&_nc_ht=scontent.fdac19-1.fna&oh=00_AfBo7YMz5jQfCdkFsXKW1A9WEyWB3PudpjT-orTPl_dNdg&oe=64957B35",
+
+    NoOfreact   : "55",
+    postStatus  : "public/private",
+
+    // ============comment start============//
+    comments    :[
+
+         // ============comment no -1============//
+        {
+            id         : 1,
+            name       : "json",
+            commentContent: "This unlucky man",
+            commentsReact:2,
+            commentTime: "20m ago",
+             // ============Reply start============//
+            commemtReply: [
+                {
+                    Id      : 1,
+                    Name :"Thomas",
+                    replyContant:"yes you true",
+                    replyReact:0,
+                    replyTime:"2m ago",
+                    replyShare: 0,
+
+                }, 
+            ],
+
+             // ============reply end============//
+        },
+
+
+    // ============Comment end============//
+
+    ],
+    PostShare: ["kabir","jallil"],
+    author    :{
+        authorName:"Chanel i",
+        profilePic: "https://scontent.fdac19-1.fna.fbcdn.net/v/t1.6435-9/64618358_2282460041848003_6967510150475153408_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEHEAdtzZfQffwiS_P0V5k6Cq0bTSrvuC4KrRtNKu-4LqMTCGXtCJuSELtC42sugwe9-hBYwnr4v20fxgaWglrq&_nc_ohc=Z5D8hN8y7RgAX87JFhi&_nc_ht=scontent.fdac19-1.fna&oh=00_AfDpsMAPY4oxt--O2RU5Bv6ponYOUqmuPVZ7Pxzud4yaTA&oe=64B7855A",
+        postTime: "June 19, 2023",
+        
+    }
+
+},
+
+ //========== post no 1===================//
+ {
+    id          :1,
+    PostContant :"Spain win the 2023 UEFA Nations League 🏆",
+    imageVideo  :"https://scontent.fdac19-1.fna.fbcdn.net/v/t39.30808-6/354975743_6300995636661070_4871955297895329178_n.jpg?stp=dst-jpg_s640x640&_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeH5VIW8j9dcoyC4BLub9blXGeO1QzA1e-oZ47VDMDV76ieEdlh8qjPj4Q6H-hbx709CCgsmGzc0KND79CLNFYWZ&_nc_ohc=S0pSh74g6iAAX_qZswI&_nc_ht=scontent.fdac19-1.fna&oh=00_AfAT_MEemWMHhGwvRtaKMAbKPn2cK1Wt_j-AUyU4Kab0dw&oe=6494AAA7",
+
+    NoOfreact   : 13,
+    postStatus  : "public/private",
+    //========== comment start===================//
+    comments    : [
+        {
+            id         : 1,
+            name       : "johurul",
+            commentContent: "Congratulation",
+            commentsReact:2,
+            commentTime: "2h ago",
+            commemtReply:[{
+                id      :1,
+                name    :"jahid",
+                replyContant:"Thank you",
+                replyReact: 2,
+                replyTime:"1h ago",
+                replyShare:0,
+
+                },
+            ],
+
+        },
+    ],
+     //========== comment end===================//
+
+    PostShare: ["jahid","hasan","walid"],
+    author    :{
+        authorName:"Kawsir Hossain",
+        profilePic: "https://scontent.fdac19-1.fna.fbcdn.net/v/t1.6435-9/64618358_2282460041848003_6967510150475153408_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEHEAdtzZfQffwiS_P0V5k6Cq0bTSrvuC4KrRtNKu-4LqMTCGXtCJuSELtC42sugwe9-hBYwnr4v20fxgaWglrq&_nc_ohc=Z5D8hN8y7RgAX87JFhi&_nc_ht=scontent.fdac19-1.fna&oh=00_AfDpsMAPY4oxt--O2RU5Bv6ponYOUqmuPVZ7Pxzud4yaTA&oe=64B7855A",
+        postTime: "June 18, 2019 ",
+        
+    },
+
+   
+},
+
+//=================facebook post no 4===================//
+
+
+{
+
+    id          :4,
+    PostContant :"ঈদের ‍ছুটি বাড়লো ১ দিন।",
+    imageVideo  :"https://scontent.fdac19-1.fna.fbcdn.net/v/t39.30808-6/354574440_820596692767860_447431839093913637_n.jpg?stp=dst-jpg_p526x296&_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_eui2=AeGNobV4i-879xPBicdBudjwH0i2WJ5L16MfSLZYnkvXozL5okc54EpZ7ppzd5oAQwRHsAP3KdSIWmu6elC6BeZA&_nc_ohc=LCL8Pq8z8kQAX_TgbFL&_nc_ht=scontent.fdac19-1.fna&oh=00_AfCZcfMtf3b6wI7oC8NN_N8SP6zfA8sViSgCey9pV2iWJg&oe=649526E0",
+
+    NoOfreact   : "560",
+    postStatus  : "public/private",
+
+    // ============comment start============//
+    comments    :[
+
+         // ============comment no -1============//
+        {
+            id         : 1,
+            name       : "Sumaiya Jannat Madani",
+            commentContent: "অবিবাহিত'রা খুশি হয়ে লাভ নেই 🙂",
+            commentsReact:8,
+            commentTime: "46m ago",
+             // ============Reply start============//
+            commemtReply: [
+                {
+                    Id      : 1,
+                    Name :"ক্বারী আবু নাঈম মাহমুদী",
+                    replyContant:"Sumaiya Jannat Madani এরকম ডাহা মিছা কথা কেমনে কন 🙄",
+                    replyReact:0,
+                    replyTime:"42m ago",
+                    replyShare: 0,
+
+                }, 
+                 // ============reply end============//
+            ],
+
+             // ============comment end============//
+        },
+
+        // ============comment no -2 ============//
+        {
+            id         : 2,
+            name       : "ইসলামের আলো",
+            commentContent: "ইনশাআল্লাহ আগামী ২৯ জুন বাংলাদেশে পবিত্র ঈদুল আজহা অনুষ্ঠিত হবে। ইসলামিক পেজটি ফলো দেওয়ার অনুরোধ রইল।",
+            commentsReact:0,
+            commentTime: "30m ago",
+                // ============Reply start============//
+            commemtReply: [
+                {
+                    Id      : 1,
+                    Name :"",
+                    replyContant:"",
+                    replyReact:0,
+                    replyTime:" ",
+                    replyShare: 0,
+
+                }, 
+            ],
+
+                // ============reply end============//
+        },
+
+
+    // ============Comment end============//
+
+    ],
+    PostShare: 21,
+    author    :{
+        authorName:"NTV",
+        profilePic: "https://scontent.fdac19-1.fna.fbcdn.net/v/t1.6435-9/64618358_2282460041848003_6967510150475153408_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEHEAdtzZfQffwiS_P0V5k6Cq0bTSrvuC4KrRtNKu-4LqMTCGXtCJuSELtC42sugwe9-hBYwnr4v20fxgaWglrq&_nc_ohc=Z5D8hN8y7RgAX87JFhi&_nc_ht=scontent.fdac19-1.fna&oh=00_AfDpsMAPY4oxt--O2RU5Bv6ponYOUqmuPVZ7Pxzud4yaTA&oe=64B7855A",
+        postTime: "June 19, 2023",
+        
+    }
+
+},
+
+
+];
+
+linkedinPostData[
+    {
+        id              :1,
+
+        postContant     :"Discover the industry’s most versatile modular evaluation platform providing a flexible set of building blocks for in-circuit evaluation of Silicon Carbide MOSFETs within minutes.",
+
+        postMedia: "https://media.licdn.com/dms/image/sync/D5618AQEEwQ3Bz7lxPw/companyUpdate-article-image-shrink_627_1200/0/1673366910269/SpeedValKitInteractiveheroimagejpg?e=1689811200&v=beta&t=GcSIomTeN0LDUaOHAOgC5RglA0A-Nebpp6eXShQRC5M",
+        like        :1695,
+        comment     :{
+            id          :1,
+            name        : "Lead Sender",
+            content     : "🔥Fuel Your Sales Growth! 📈Reach your target B2B audience effortlessly with our tailored Cold Emailing and Calling Lists.Need assistance?Lets Unlock Your Business Full Potential Together!",
+            like        :2,
+            timeDate    :"1w ago",
+            reply       :{
+                id          :1,
+                name        :"xyz",
+                content     :"tailored Cold Emailing and Calling",
+                like        :2,
+            }
+
+        },
+
+        repots      :{
+            id          :1,
+            name        : "johurul islam",
+            content     : "",
+        },
+        autor       :{
+            id          :1,
+            name        : "Wolfspeed",
+            profilePic  : "",
+
+        }
+
+
+    }
+];
+
